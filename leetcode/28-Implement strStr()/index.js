a4e60b85ed61e538635358f288a7db6b2aac1080
Deng XiaoLong <Dragonet_d@163.com>
2019-07-21 12:40:00 +0800

    leetcode implement strStr()

---
+++ leetcode/28-Implement strStr()/index.js
@@ -0,0 +1,10 @@
+function strStr (haystack, needle) {
+    for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(strStr('hello', 'll'));
