23624ce45adc758fbeeadf4da753c05758b192fa
Deng XiaoLong <Dragonet_d@163.com>
2019-11-24 17:21:56 +0800

    :green_heart: binary tree level order travers

---
+++ leetcode/107-BinaryTreeLevelOrderTravers-2/index.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+from typing import List
+class TreeNode:
+  def __init__(self, x):
+    self.val = x
+    self.left = None
+    self.right = None
+
+
+class Solution:
+  def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+     if root is None:
+       return []
