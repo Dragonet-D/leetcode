e0f57f0752e38522ff792e55fe2824234189e2b3
Deng XiaoLong <Dragonet_d@163.com>
2019-12-06 22:04:25 +0800

    :heart: 70 climbing stairs

--- leetcode/70-ClimbingStairs/index.js
+++ leetcode/70-ClimbingStairs/index.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
+const climbStairs = function(n) {
   let prev = 0,current = 1;
   for (let i = 0; i < n; i++) {
     [prev, current] = [current, prev + current]
