cefc8b309ece4a962d58969732c88674591b2436
Deng XiaoLong <Dragonet_d@163.com>
2019-12-09 22:14:28 +0800

    :art: 112 path sum

--- leetcode/112-PathSum/index.py
+++ leetcode/112-PathSum/index.py
@@ -8,3 +8,9 @@ class TreeNode:
 
 class Solution:
   def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+    if root is None:
+      return False
+    if root.left is None and root.right is None and root.val == sum:
+      return True
+    else:
+      return self.hasPathSum(root.left, sum - root.val)
