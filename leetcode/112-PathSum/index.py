514ad02a2d812d1a8a1d48156316ead75862663a
Deng XiaoLong <Dragonet_d@163.com>
2019-12-09 22:15:31 +0800

    :art: 112 path sum

--- leetcode/112-PathSum/index.py
+++ leetcode/112-PathSum/index.py
@@ -13,4 +13,4 @@ class Solution:
     if root.left is None and root.right is None and root.val == sum:
       return True
     else:
-      return self.hasPathSum(root.left, sum - root.val)
+      return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
