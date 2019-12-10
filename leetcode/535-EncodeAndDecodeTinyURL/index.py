3cb2e92a8ad00289db55e7d5459c0502f5f35897
Deng XiaoLong <Dragonet_d@163.com>
2019-12-10 21:32:14 +0800

    :art: 535 encode and decode thiny URL

--- leetcode/535-EncodeAndDecodeTinyURL/index.py
+++ leetcode/535-EncodeAndDecodeTinyURL/index.py
@@ -1,12 +1,38 @@
+import string
+import random
+import math
+full_tiny = {}
+tiny_full = {}
+letters = string.ascii_letters + string.digits
+short_url = 'http://tinyurl.com/'
 class Codec:
 
   def encode(self, longUrl: str) -> str:
     """Encodes a URL to a shortened URL.
     """
-
+    def short_addr():
+      ans = ''
+      tmp = ''
+      for i in range(6):
+        tmp = letters[random.randint(0, 10000) % 62]
+        ans += tmp
+      return ans
+    if longUrl in full_tiny:
+      return short_url + full_tiny[longUrl]
+    else:
+      suffix = short_addr()
+      full_tiny[suffix] = longUrl
+      full_tiny[longUrl] = suffix
+      return short_url + suffix
   def decode(self, shortUrl: str) -> str:
     """Decodes a shortened URL to its original URL.
     """
+    shortUrl = shortUrl.split('/')[-1]
+    if shortUrl in full_tiny:
+      return full_tiny[shortUrl]
+    else:
+      return ''
+
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
