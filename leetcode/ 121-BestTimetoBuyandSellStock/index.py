b2b0b91dabbbf4645178d2b25a9a14c63fca3560
Deng XiaoLong <Dragonet_d@163.com>
2019-12-15 22:42:56 +0800

    :art: 121 best time to buy and sell socket

---
+++ leetcode/ 121-BestTimetoBuyandSellStock/index.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+  def maxProfit(self, prices: List[int]) -> int:
+    maxProft, minPrice = 0, float('inf')
+
+    for price in prices:
+      minPrice = min(price, minPrice)
+      maxProft = max(maxProft, price - minPrice)
+
+    return maxProft
