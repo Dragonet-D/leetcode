1484989056bdccf755bb71dbc389ee0b0efb3fa7
Dragonet-D <Dragonet_D@163.com>
2020-01-09 13:16:23 +0800

    :art: 21 merge two sorted lists

--- leetcode/21-Merge-Two-Sorted-Lists/index.py
+++ leetcode/21-Merge-Two-Sorted-Lists/index.py
@@ -18,3 +18,9 @@ class Solution:
     curr.next = l1 or l2
 
     return dummy.next
+
+  def mergeTwoLists2(self, l1: ListNode, l2: ListNode) -> ListNode:
+    if l1 and l2:
+      if l1.val > l2.val : l1, l2 = l2, l1
+      l1.next = self.mergeTwoLists2(l1.next, l2)
+    return l1 or l2
