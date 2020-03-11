c37a9c8ee808cc82fc13fab6da1363909196d241
Dragonet-D <Dragonet_D@163.com>
2020-03-11 22:23:28 +0800

    :robot:

---
+++ leetcode/88-MergeSortedArray/index.js
@@ -0,0 +1,21 @@
+var merge = function(nums1, m, nums2, n) {
+  let p1 = m - 1
+  let p2 = n - 1
+  let p = m + n - 1
+
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums1[p1] > nums2[p2]) {
+      nums1[p] = nums1[p1]
+      p--
+      p1--
+    } else {
+      nums1[p] = nums2[p2]
+      p--
+      p2--
+    }
+  }
+
+  if (p2 >= 0) {
+    nums1.splice(0, p2 + 1, ...nums2.slice(0, p2 + 1))
+  }
+};
