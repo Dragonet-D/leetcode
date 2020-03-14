e51dfb1cdb94cf6e2b991e5d3bfd9476dab47b75
Dragonet-D <Dragonet_D@163.com>
2020-03-14 16:12:11 +0800

    :art: algorithm

--- leetcode/1071-字符串的最大公因子/index.py
+++ leetcode/1071-字符串的最大公因子/index.py
@@ -4,7 +4,12 @@ from math import gcd
 class Solution:
   def gcdOfStrings(self, str1: str, str2: str) -> str:
     a = gcd(len(str1), len(str2))
+    print(a)
     b = str1[:a]
+    print(b)
     if str1 + str2 == str2 + str1:
       return b
     return ''
+
+a = Solution()
+print(a.gcdOfStrings('ababab', 'abab'))
