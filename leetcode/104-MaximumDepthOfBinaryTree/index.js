5d18f8417eb76f515aeea725a84d2cc47fca6344
dragonet-d <Dragonet_D@163.com>
2020-07-10 20:01:36 +0800

    :heart: 二叉树的深度

--- leetcode/104-MaximumDepthOfBinaryTree/index.js
+++ leetcode/104-MaximumDepthOfBinaryTree/index.js
@@ -4,3 +4,18 @@ const maxDepth = root => {
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
   }
 }
+
+const maxDepth1 = root => {
+  if (!root) {
+    return 0
+  } else {
+    const left = maxDepth1(root.left)
+    const right = maxDepth1(root.right)
+
+    return Math.max(left, right) + 1
+  }
+}
+
+function treeNode(value) {
+
+}
